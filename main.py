7ea6a73eb4167e6ca34f53e6125f4ded572bf064
d-gg-r <132926710+d-gg-r@users.noreply.github.com>
2023-09-04 20:01:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-# required: pip install deepface matplotlib
+# Copyright (c) 2023 tb38raco, d_gg_r
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
+# files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, 
+# modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software 
+# is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE 
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+ #CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+# Benötigt: pip install deepface matplotlib 
 import os
 
 from deepface import DeepFace
 import cv2
 import matplotlib.pyplot as plt
 from fer import FER
+from tabulate import tabulate  # Installiere die tabulate-Bibliothek mit 'pip install tabulate'
 
 # Pfad zum Ordner mit den Bildern
 folder_path = 'IMG_1001.JPG'
@@ -22,7 +37,6 @@ show_age = True
 # Liste, um die Ergebnisse der Gesichtsverifikation zu speichern
 results = []
 
-
 # Durchlaufe alle Bilder im Ordner
 files = os.listdir(folder_path)
 for i in range(len(files)):
@@ -36,13 +50,31 @@ for i in range(len(files)):
 
         # Ergebnis zur Ergebnisliste hinzufügen
         results.append((files[i], files[j], result["distance"], result["verified"]))
-        
- # Ergebnisse ausgeben
+        # Erstellen Sie eine Tabelle mit den Ergebnissen
+        table_headers = ["Bild 1", "Bild 2", "Distanz", "Wahrheitswert"]
+        table_data = results
+
+        # Tabelle anzeigen
+        print(tabulate(table_data, headers=table_headers, tablefmt="pretty"))
+ # Festlegen des Schwellenwerts für die Gesichtsverifikation
+        threshold = 0.6  # Ändern Sie diesen Schwellenwert nach Bedarf
+
+        # Überprüfe, ob der Wahrheitswert basierend auf dem Schwellenwert True oder False ist
+        updated_table_data = []
+        for row in table_data:
+            distance = row[2]
+            is_verified = distance < threshold
+            updated_table_data.append([row[0], row[1], distance, is_verified])
+
+        # Aktualisierte Tabelle anzeigen
+        print("\nAktualisierte Tabelle mit dem Schwellenwert:")
+        print(tabulate(updated_table_data, headers=table_headers, tablefmt="pretty"))
+
+# Ergebnisse ausgeben
 print("Bild 1\t\tBild 2\t\tDistanz\t\tWahrheitswert")
 print("------------------------------------------------------------")
 for filename1, filename2, distance, verified in results:
     print(f"{filename1}\t\t{filename2}\t\t{distance}\t\t{verified}")
-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
 
 # Kamera initialisieren
 camera = cv2.VideoCapture(0)
@@ -79,11 +111,10 @@ while True:
             # Nur den grünen Kasten anzeigen
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
             
-            
- # Livestream anzeigen
+# Livestream anzeigen
     cv2.imshow('Emotion and Age Detection', frame)
-    
-  # Tastenabfrage
+
+# Tastenabfrage
     key = cv2.waitKey(1)
     if key == ord('q') or key == ord('Q'):  # 'q' drücken, um die Schleife zu beenden
         break
@@ -92,6 +123,6 @@ while True:
     elif key == ord('a') or key == ord('A'):  # 'a' drücken, um das Alter ein- oder auszublenden
         show_age = not show_age
         
- # Kameraressourcen freigeben
+# Kameraressourcen freigeben
 camera.release()
 cv2.destroyAllWindows()
